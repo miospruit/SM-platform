8b119b1bc82b2c19a78750d35b0b349dcab576be
Mio Spruit <mio12@hotmail.nl>
2019-09-21 15:17:53 +0200

    Add about and contact page.
    Adding about and contact page using blade and @include.

---
+++ MS-platform/resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>About</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Dolore obcaecati fuga, sunt iusto reprehenderit quia molestiae necessitatibus nobis dolores,
+    animi eligendi beatae ex ducimus libero sint facere fugit nesciunt veniam.</p>
+
+@endsection
