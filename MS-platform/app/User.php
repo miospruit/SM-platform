2d29894c4a5efeaff73cff7373bc5e3ca466c0fa
Mio Spruit <mio12@hotmail.nl>
2019-10-22 15:52:14 +0200

    Add user model with relations.

--- MS-platform/app/User.php
+++ MS-platform/app/User.php
@@ -5,9 +5,26 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\model;
 
 class User extends Authenticatable
 {
+
+    public function Photos()
+    {
+        return $this->hasMany('App\Photo');
+    }
+
+    public function Comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    public function Likes()
+    {
+        return $this->hasMany('App/Like');
+    }
+
     use Notifiable;
 
     /**
