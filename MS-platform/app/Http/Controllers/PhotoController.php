b8869b8e66a80a33716e6839120deb3e10235ec7
Mio Spruit <mio12@hotmail.nl>
2019-10-30 14:16:20 +0100

    Fix PhotoController.
    Add storage and edit.

--- MS-platform/app/Http/Controllers/PhotoController.php
+++ MS-platform/app/Http/Controllers/PhotoController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Photo;
 use Illuminate\Http\Request;
-use Illuminate\Routing\RedirectController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-use phpDocumentor\Reflection\DocBlock\Description;
 
 class PhotoController extends Controller
 {
@@ -36,7 +34,11 @@ class PhotoController extends Controller
      */
     public function create()
     {
-        return view('photos.create');
+        if (Auth::check()) {
+            return view('photos.create');
+        } else {
+            return view('photos.index');
+        }
     }
 
     /**
@@ -47,33 +49,38 @@ class PhotoController extends Controller
      */
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'title' => ['required','max:255'],
-            'description' => ['required'],
-            'image' => ['required','image','mimes:jpeg,png,jpg','max:2048']
-        ]);
+        //check if user is logged in.
+        if (Auth::check()) {
 
-        if ($files = $request->file('image'))
-        {
-            // Generate a file name with extension
-            $userId = Auth::user()->id;
-            $fileName = $userId.'_post'.time().'.'.$files->getClientOriginalExtension();
-            // Save the file
-            $path = $files->storeAs('/public/photos', $fileName);
-
-            // $path = $request->file('image')->store('storage');
-
-            Photo::create([
-                'user_id' => Auth::user()->id,
-                'title' => $request->title,
-                'description' => $request->description,
-                'image' => $fileName
+            //validate request if data is correct
+            $validatedData = $request->validate([
+                'title' => ['required', 'max:255'],
+                'description' => ['required'],
+                'image' => ['required', 'image', 'mimes:jpeg,png,jpg', 'max:2048']
             ]);
 
+            //check if the request has a file 'image'
+            if ($files = $request->file('image')) {
+                // Generate a file name with extension
+                $userId = Auth::user()->id;
+                $fileName = $userId . '_post' . time() . '.' . $files->getClientOriginalExtension();
+                // Save the file
+                $path = $files->storeAs('/public/photos', $fileName);
+
+                // $path = $request->file('image')->store('storage');
+
+                Photo::create([
+                    'user_id' => Auth::user()->id,
+                    'title' => $request->title,
+                    'description' => $request->description,
+                    'image' => $fileName
+                ]);
+            }
+
+            return redirect("photos");
+        } else {
+            //add else code
         }
-
-        return redirect("photos");
-
     }
 
 
@@ -85,7 +92,13 @@ class PhotoController extends Controller
      */
     public function show(Photo $photo)
     {
-        return view('photos.show', compact('photo'));
+
+        // if (Auth::user()->id == $photo->user_id) {
+        // return true;
+        // }
+        $comments = $photo->comments;
+        return view('photos.show', compact('photo', 'comments'));
+        // return false;
     }
 
     /**
@@ -96,9 +109,21 @@ class PhotoController extends Controller
      */
     public function edit(Photo $photo)
     {
+        // If user is administrator, then can edit any post
+        // if ($user->isModerator()) {
+        //     return true;
+        // }
 
+        // Check if user is the post author
+        // if (Auth::user()->id == $photo->user_id) {
+        // // dd(Auth::user()->id);
+        //     return true;
+        // }
+
+        // return false;
         // add check of role and person
         // $photo = Photo::find($photo);
+        // dd($photo);
         return view('photos.edit', compact('photo'));
     }
 
@@ -112,10 +137,14 @@ class PhotoController extends Controller
     public function update(Request $request, Photo $photo)
     {
 
+        request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
         $photo->update(request(['title', 'description']));
 
         return redirect('/photos');
-
     }
 
     /**
