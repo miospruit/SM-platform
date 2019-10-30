c240435b56ef32edce37fba6d9f3a377c94b28b3
Mio Spruit <mio12@hotmail.nl>
2019-10-30 22:06:37 +0100

    Add fontawsome

--- MS-platform/resources/views/layouts/app.blade.php
+++ MS-platform/resources/views/layouts/app.blade.php
@@ -1,109 +1,125 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="https://kit.fontawesome.com/e9839cb680.js" crossorigin="anonymous"></script>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-        <nav class="navbar" role="navigation" aria-label="main navigation">
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+
+    <body>
+        <div id="app">
+
+            <nav class="navbar" role="navigation" aria-label="main navigation">
                 <div class="navbar-brand">
-                  <a class="navbar-item" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                  </a>
+                    <a class="navbar-item" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
 
 
-                  <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                  </a>
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+                        data-target="navMenu">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
                 </div>
 
                 <div id="navMenu" class="navbar-menu">
-                  <div class="navbar-start">
-                    <a class="navbar-item" href="/photos/create">
-                      Create new post
-                    </a>
-                    <a class="navbar-item" href="/photos">
-                        Wall
-                      </a>
-
-                    <hr class="navbar-divider">
-
-                    <div class="navbar-item has-dropdown is-hoverable">
-                      <a class="navbar-link">
-                        More
-                      </a>
-
-                      <div class="navbar-dropdown">
-                        <a class="navbar-item" href="/account">
-                          Account
+                    <div class="navbar-start">
+                        <a class="navbar-item" href="/photos/create">
+                            Create new post
                         </a>
-                        <a class="navbar-item" href="/layouts/app">
-                          Contact
+                        <a class="navbar-item" href="/photos">
+                            Wall
                         </a>
-                      </div>
-                    </div>
-                  </div>
 
-                  <div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="buttons">
-                        @guest
-                        @if (Route::has('register'))
-                            <a class="button is-primary" href="{{ route('register') }}">
-                                {{ __('Register') }}
-                                {{-- <strong>Sign up</strong> --}}
-                            </a>
-                        @endif
-                            <a class="button is-light" href="{{ route('login') }}">
-                                {{ __('Login') }}
-                            {{-- Log in --}}
-                            </a>
-                        </div>
-                        @else
+                        <hr class="navbar-divider">
+
                         <div class="navbar-item has-dropdown is-hoverable">
                             <a class="navbar-link">
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                More
                             </a>
 
                             <div class="navbar-dropdown">
-                                <a class="navbar-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                                    {{ __('logout') }}
+                                <a class="navbar-item" href="/account">
+                                    Account
                                 </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
+                                <a class="navbar-item" href="/layouts/app">
+                                    Contact
+                                </a>
                             </div>
-
-                            @endguest
                         </div>
                     </div>
-                  </div>
-                </div>
-              </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <div class="buttons">
+                                @guest
+                                @if (Route::has('register'))
+                                <a class="button is-primary" href="{{ route('register') }}">
+                                    {{ __('Register') }}
+                                    {{-- <strong>Sign up</strong> --}}
+                                </a>
+                                @endif
+                                <a class="button is-light" href="{{ route('login') }}">
+                                    {{ __('Login') }}
+                                    {{-- Log in --}}
+                                </a>
+                            </div>
+                            @else
+                            <div class="navbar-item has-dropdown is-hoverable">
+                                <a class="navbar-link">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="navbar-dropdown">
+                                    <a class="navbar-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                        {{ __('logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+
+                                @endguest
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <main class="py-4">
+                @yield('content')
+            </main>
+            <footer class="footer">
+                <div class="content has-text-centered">
+                    <p>
+                        <strong>Social media</strong> by <a href="#">Mio Spruit</a>. The source code is
+                        licensed
+                        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                    </p>
+                </div>
+            </footer>
+        </div>
+    </body>
+
 </html>
